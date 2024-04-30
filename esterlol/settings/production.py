ec12ddeb757c74f398adf3bf75aa04c581966a70
Etty <beltrami.ester@gmail.com>
2024-04-30 21:22:35 +0100

    Get SECRET_KEY from env

--- esterlol/settings/production.py
+++ esterlol/settings/production.py
@@ -1,11 +1,18 @@
 from .base import *  # noqa
 
+import environ
+
 import contextlib
 
+env = environ.Env()
+
 DEBUG = False
+SECRET_KEY = env("SECRET_KEY")
+
 ALLOWED_HOSTS = [
     "ester-lol.fly.dev",
 ]
+
 WAGTAILADMIN_BASE_URL = "https://esterlol.fly.dev"
 
 with contextlib.suppress(ImportError):
