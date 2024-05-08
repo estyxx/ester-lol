49d0fd51e2087c1dbd06e197352a25d87ee658dd
Etty <beltrami.ester@gmail.com>
2024-05-08 12:03:03 +0100

    Add SpeedInsights

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,6 +1,7 @@
 import Script from 'next/script';
 import { Montserrat } from 'next/font/google';
 import type { Metadata } from 'next';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import { Analytics } from '@vercel/analytics/react';
 
 import './globals.css';
@@ -51,6 +52,7 @@ export default function RootLayout({
         >
           {children}
           <Analytics />
+          <SpeedInsights />
         </main>
         <Footer />
       </body>
