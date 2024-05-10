25d01b3e3e2d8f6cd1d04d2559a76352bcb0ace8
Etty <beltrami.ester@gmail.com>
2024-05-10 13:12:50 +0100

    Update skills

--- frontend/components/skills.tsx
+++ frontend/components/skills.tsx
@@ -14,22 +14,26 @@ const Skill = ({ name, x, y }: { name: string; x: string; y: string }) => {
 
 const Skills = () => {
   return (
-    <div className='hidden xl:block'>
-      <h2 className='font-bold text-8xl mt-64 mb-8 w-full text-center dark:text-light '>
+    <div className='hidden xl:block my-16 lg:my-32'>
+      <h2 className='font-bold text-8xl mb-8 lg:mb-32 w-full text-center dark:text-light '>
         Skills
       </h2>
-      <div className='w-full h-screen relative flex items-center justify-center rounded-full bg-circularLight dark:bg-circularDark'>
-        <Skill name='Python' x='0vw' y='0vw' />
-        <Skill name='GraphQL' x='-20vw' y='2vw' />
-        <Skill name='Django' x='7vw' y='3vw' />
-        <Skill name='ReactJS' x='-8vw' y='5vw' />
-        <Skill name='Typescript' x='0vw' y='10vw' />
-        <Skill name='NextJS' x='15vw' y='10vw' />
-        <Skill name='Tailwind CSS' x='-18vw' y='18vw' />
-        <Skill name='Wagtail CMS' x='-8vw' y='-20vw' />
-        <Skill name='CSS' x='25vw' y='18vw' />
-        <Skill name='Storybook' x='25vw' y='18vw' />
-        <Skill name='Accessibility' x='-25vw' y='-18vw' />
+      <div className='flex items-center justify-center'>
+        <div className='xl:w-full 3xl:w-[80%] h-screen relative flex items-center justify-center rounded-full bg-circularLight dark:bg-circularDark'>
+          <Skill name='Python' x='0vw' y='0vw' />
+          <Skill name='GraphQL' x='-5vw' y='-10vw' />
+          <Skill name='Django' x='7vw' y='3vw' />
+          <Skill name='Web Scraping' x='12vw' y='18vw' />
+          <Skill name='React' x='18vw' y='-5vw' />
+          <Skill name='Typescript' x='-4vw' y='10vw' />
+          <Skill name='NextJS' x='15vw' y='10vw' />
+          <Skill name='Tailwind CSS' x='-18vw' y='-2vw' />
+          <Skill name='CSS' x='-11vw' y='2vw' />
+          <Skill name='Wagtail CMS' x='-8vw' y='-20vw' />
+          <Skill name='Storybook' x='-18vw' y='18vw' />
+          <Skill name='Accessibility' x='10vw' y='-15vw' />
+          <Skill name='Public Speaking' x='-0vw' y='20vw' />
+        </div>
       </div>
     </div>
   );
